d39137c1189b8eb00e7c7f456b5e8f3656264e18
eytan1998 <eytan1998@gmail.com>
2024-01-31 20:59:33 +0200

    make intro better

--- app/src/main/java/com/example/minyan/Activity/IntroActivity.java
+++ app/src/main/java/com/example/minyan/Activity/IntroActivity.java
@@ -2,6 +2,10 @@ package com.example.minyan.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.minyan.R;
 
@@ -12,6 +16,11 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        //setup animation
+        Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+        ((ImageView) findViewById(R.id.imageViewIntroMarker)).startAnimation(anim);
+
 
         findViewById(R.id.buttonIntroPrayer).setOnClickListener(v -> {
             Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
